44ea9e7187d8dab7f8909faa32e8f07fa379d385
Charles Daniels <charles@cdaniels.net>
2020-05-26 16:17:11 -0400

    fix multiple plot instances, plot size
    * It looks like the plot widget ignores it's size parameter. I worked
      around this by having imgui render the plot within a Child which had
      the desired size.
    * The issues I was encountering previously with multiple child widgets
      were caused because I was ignoring the return of BeginPlot.

--- Widgets.go
+++ Widgets.go
@@ -119,13 +119,16 @@ type PlotLinesWidget struct {
 
 func (p *PlotLinesWidget) Build() {
 	// imgui.PlotLinesV(p.label, p.values, p.valuesOffset, p.overlayText, p.scaleMin, p.scaleMax, p.graphSize)
-	imgui.BeginPlot(p.label, "x label", "y label", p.graphSize, 0, 0, 0, 0, 0)
-	imgui.Plot(p.label+"plot", p.values, len(p.values), 0, 4)
-	imgui.EndPlot()
+	imgui.BeginChildV(p.label+"container", p.graphSize, true, 0)
+	if imgui.BeginPlot(p.label, "x label", "y label", p.graphSize, 0, 0, 0, 0, 0) {
+		imgui.Plot(p.label, p.values, len(p.values), 0, 4)
+		imgui.EndPlot()
+	}
+	imgui.EndChild()
 }
 
 func PlotLines(label string, values []float32) *PlotLinesWidget {
-	return PlotLinesV(label, values, 0, "", math.MaxFloat32, math.MaxFloat32, 0, 0)
+	return PlotLinesV(label, values, 0, "", math.MaxFloat32, math.MaxFloat32, 640, 480)
 }
 
 func PlotLinesV(label string, values []float32, valuesOffset int, overlayText string, scaleMin, scaleMax, width, height float32) *PlotLinesWidget {
--- examples/implot/main.go
+++ examples/implot/main.go
@@ -19,11 +19,11 @@ func loop() {
 		g.Label("Simple sin(x) plot:"),
 		g.PlotLines("testplot", plotdata),
 		g.Label("sin(x) plot with overlay text, and size:"),
-		// g.PlotLinesV("plot label", plotdata, 0, "overlay text", math.MaxFloat32, math.MaxFloat32, 500, 200),
+		g.PlotLinesV("plot label", plotdata, 0, "overlay text", math.MaxFloat32, math.MaxFloat32, 500, 200),
 	})
 }
 
 func main() {
-	wnd := g.NewMasterWindow("Hello world", 600, 400, g.MasterWindowFlagsNotResizable, nil)
+	wnd := g.NewMasterWindow("Hello world", 600, 800, g.MasterWindowFlagsNotResizable, nil)
 	wnd.Main(loop)
 }
